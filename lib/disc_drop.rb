aad87ef4e4ef9b68f41fac31121981134c17b840
Erik-Russell <erik.z.russell@gmail.com>
2024-10-13 12:15:38 -0700

    testing iteration

--- lib/disc_drop.rb
+++ lib/disc_drop.rb
@@ -2,4 +2,8 @@
 
 # DiscDrop - game of Connect Four
 class DiscDrop
+  attr_reader :game_over
+  def initialize
+    @game_over = false
+  end
 end
--- spec/disc_drop_spec.rb
+++ spec/disc_drop_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 require_relative '../lib/disc_drop.rb'
 
-RSpec.describe 'Example' do
-  it 'does something' do
-    expect(1 + 1).to eq(2)
-  end
-end
-
 # describe whole class
 # set subject to be new instance of class
 # first test is there is a new instance of the class
 describe DiscDrop do
-  it 'creates an instance of DiscDrop' do
-    instance = DiscDrop.new
-    expect(instance).to be_an_instance_of(DiscDrop)
+  describe '#initialize' do
+    subject = DiscDrop.new
+    it 'creates an instance of DiscDrop' do
+      expect(subject).to be_an_instance_of(DiscDrop)
+    end
+
+    it 'game over is false' do
+      expect(subject.game_over).to be false
+    end
   end
 end
