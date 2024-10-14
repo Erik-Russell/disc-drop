e2ced742fa3302ae02d570caed7045b8ad40e6c5
Erik-Russell <erik.z.russell@gmail.com>
2024-10-14 10:12:48 -0700

    #welcome_message

--- lib/disc_drop.rb
+++ lib/disc_drop.rb
@@ -5,5 +5,11 @@ class DiscDrop
   attr_reader :game_over
   def initialize
     @game_over = false
+    @columns = 7
+    @rows = 6
+  end
+
+  def welcome_message
+    puts 'Welcome to DiscDrop, a 7,6,4 m,n,k game.'
   end
 end
