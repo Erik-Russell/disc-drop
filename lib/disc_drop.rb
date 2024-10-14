efaa34d6a47fd81be02232fd381e9cbbb16a5f17
Erik-Russell <erik.z.russell@gmail.com>
2024-10-14 12:19:59 -0700

    Added board and method to display

--- lib/disc_drop.rb
+++ lib/disc_drop.rb
@@ -2,14 +2,20 @@
 
 # DiscDrop - game of Connect Four
 class DiscDrop
-  attr_reader :game_over
+  attr_reader :game_over, :board
   def initialize
     @game_over = false
     @columns = 7
     @rows = 6
+    @board = Array.new(6) { Array.new(7, '_') }
   end
 
   def welcome_message
     puts 'Welcome to DiscDrop, a 7,6,4 m,n,k game.'
   end
+
+  def display_board
+    @board.each_with_index do |row, index|
+      puts row.join(' | ')
+    end
 end
