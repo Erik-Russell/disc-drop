138036b6f8db36f57cca2bb8c5e4ec017a730396
Erik-Russell <erik.z.russell@gmail.com>
2024-10-14 16:07:47 -0700

    Added methods for dropping in a disc

--- lib/disc_drop.rb
+++ lib/disc_drop.rb
@@ -8,6 +8,7 @@ class DiscDrop
     @columns = 7
     @rows = 6
     @board = Array.new(6) { Array.new(7, '_') }
+    @current_player = 'x'
   end
 
   def welcome_message
@@ -18,4 +19,35 @@ class DiscDrop
     @board.each_with_index do |row, index|
       puts row.join(' | ')
     end
+  end
+
+  def drop_disc(column,player)
+    unless valid_input?(column)
+      puts "invalid input"
+      return nil
+    end
+
+    dropped = false
+
+    @board.reverse_each do |row|
+      next if row[column] != '_'
+      break if dropped == true
+      row[column] = @current_player
+      dropped = true
+    end
+
+    unless dropped
+      puts 'column full'
+    end
+  end
+
+  def switch_player
+    @current_player = @current_player == 'x' ? 'o' : 'x'
+  end
+
+  def valid_input?(input)
+    return false unless input.is_a? Numeric
+    input.between?(0,6)
+  end
+
 end
