1d0b4c060c9c58154d9e3c9897d6ce8bcf572e46
Erik-Russell <erik.z.russell@gmail.com>
2024-10-15 11:09:12 -0700

    Added win logic

--- lib/disc_drop.rb
+++ lib/disc_drop.rb
@@ -2,12 +2,12 @@
 
 # DiscDrop - game of Connect Four
 class DiscDrop
-  attr_reader :game_over, :board
+  attr_reader :game_over, :board, :current_player
   def initialize
     @game_over = false
     @columns = 7
     @rows = 6
-    @board = Array.new(6) { Array.new(7, '_') }
+    @board = Array.new(@rows) { Array.new(@columns, '_') }
     @current_player = 'x'
   end
 
@@ -16,12 +16,12 @@ class DiscDrop
   end
 
   def display_board
-    @board.each_with_index do |row, index|
+    @board.each do |row|
       puts row.join(' | ')
     end
   end
 
-  def drop_disc(column,player)
+  def drop_disc(column)
     unless valid_input?(column)
       puts "invalid input"
       return nil
@@ -30,10 +30,11 @@ class DiscDrop
     dropped = false
 
     @board.reverse_each do |row|
-      next if row[column] != '_'
-      break if dropped == true
-      row[column] = @current_player
-      dropped = true
+      if row[column] == '_'
+        row[column] = @current_player
+        dropped = true
+        break
+      end
     end
 
     unless dropped
@@ -47,7 +48,54 @@ class DiscDrop
 
   def valid_input?(input)
     return false unless input.is_a? Numeric
-    input.between?(0,6)
+    input.between?(0, 6)
   end
 
+  def horizontal_win?
+    @board.each do |row|
+      if row.join.include?('xxxx') || row.join.include?('oooo')
+        return true
+      end
+    end
+    false
+  end
+
+  def vertical_win?
+    @board.transpose.each do |col|
+      if col.join.include?('xxxx') || col.join.include?('oooo')
+        return true
+      end
+    end
+    false
+  end
+
+  def diagonal_win?
+    # upper-left to lower-right (↘)
+    (0..@rows - 4).each do |row|
+      (0..@columns - 4).each do |col|
+        diagonal = (0..3).map { |i| @board[row + i][col + i] }
+        if diagonal.join == 'xxxx' || diagonal.join == 'oooo'
+          return true
+        end
+      end
+    end
+    false
+  end
+
+  def reverse_diagonal_win?
+    # lower-left to upper-right (↗)
+    (3..@rows - 4).each do |row|
+      (0..@columns - 4).each do |col|
+        diagonal = (0..3).map { |i| @board[row - i][col + i] }
+        if diagonal.join == 'xxxx' || diagonal.join == 'oooo'
+          return true
+        end
+      end
+    end
+    false
+  end
+
+  def check_winner
+    horizontal_win? || vertical_win? || diagonal_win? || reverse_diagonal_win?
+  end
 end
