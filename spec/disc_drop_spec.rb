24fb37ef541a233abad54a4268651966d58717b9
Erik-Russell <erik.z.russell@gmail.com>
2024-10-16 08:59:05 -0700

    Tests for win logic and full board(draw)

--- spec/disc_drop_spec.rb
+++ spec/disc_drop_spec.rb
@@ -37,4 +37,114 @@ describe DiscDrop do
       expect(subject.board[5][5]).to eq('x')
     end
   end
+
+  describe '#check_winner' do
+    subject(:game) { DiscDrop.new }
+
+    context 'when there is a bottom-left to top-right diagonal win' do
+      before do
+        game.instance_variable_set(:@board, [
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', 'x', '_', '_', '_'],
+          ['_', '_', 'x', '_', '_', '_', '_'],
+          ['_', 'x', '_', '_', '_', '_', '_'],
+          ['x', '_', '_', '_', '_', '_', '_']
+        ])
+      end
+
+      it 'returns true for reverse_diagonal win' do
+        expect(game.check_winner).to be true
+      end
+    end
+
+    context 'when there is a top-left to bottom-right diagonal win' do
+      before do
+        game.instance_variable_set(:@board, [
+          ['o', '_', '_', '_', '_', '_', '_'],
+          ['_', 'o', '_', '_', '_', '_', '_'],
+          ['_', '_', 'o', '_', '_', '_', '_'],
+          ['_', '_', '_', 'o', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_']
+        ])
+      end
+
+      it 'returns true for diagonal win' do
+        expect(game.check_winner).to be true
+      end
+    end
+
+    context 'when there is a vertical win' do
+      before do
+        game.instance_variable_set(:@board, [
+          ['_', 'x', '_', '_', '_', '_', '_'],
+          ['_', 'x', '_', '_', '_', '_', '_'],
+          ['_', 'x', '_', '_', '_', '_', '_'],
+          ['_', 'x', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_']
+        ])
+      end
+
+      it 'returns true for a vertical win' do
+        expect(game.check_winner).to be true
+      end
+    end
+
+    context 'when there is a horizontal win' do
+      before do
+        game.instance_variable_set(:@board, [
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', 'x', 'x', 'x', 'x', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_'],
+          ['_', '_', '_', '_', '_', '_', '_']
+        ])
+      end
+
+      it 'returns true for a horizontal win' do
+        expect(game.check_winner).to be true
+      end
+    end
+  end
+
+  describe '#full_board?' do
+    subject(:no_win) { DiscDrop.new }
+
+    context 'when the board is not full' do
+      before do
+        no_win.instance_variable_set(:@board, [
+          ['o', 'o', 'x', 'x', 'o', '_', 'o'],
+          ['o', 'x', 'x', 'o', 'o', 'x', 'x'],
+          ['x', 'x', 'o', 'x', 'x', 'o', 'o'],
+          ['x', 'o', 'o', 'o', 'x', 'x', 'o'],
+          ['o', 'x', 'x', 'o', 'o', 'o', 'x'],
+          ['x', 'x', 'o', 'o', 'o', 'x', 'o']
+        ])
+      end
+
+      it 'returns false' do
+        expect(no_win.full_board?).to be false
+      end
+    end
+
+   context 'when the board is full and there is no winning move' do
+      before do
+        no_win.instance_variable_set(:@board, [
+          ['o', 'o', 'x', 'x', 'o', 'o', 'o'],
+          ['o', 'x', 'x', 'o', 'o', 'x', 'x'],
+          ['x', 'x', 'o', 'x', 'x', 'o', 'o'],
+          ['x', 'o', 'o', 'o', 'x', 'x', 'o'],
+          ['o', 'x', 'x', 'o', 'o', 'o', 'x'],
+          ['x', 'x', 'o', 'o', 'o', 'x', 'o']
+        ])
+      end
+
+      it 'returns true for a full board' do
+        expect(no_win.full_board?).to be true
+      end
+    end
+  end
 end
